618765e8f9c70b70f0398aaa6f2f11403feab1f8
j one lee <gari7222@gmail.com>
2026-08-10 15:12:56 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jone-golf-final-v1';
+const CACHE='jone-golf-final-v2';
 const ASSETS=['./', './index.html', './profile.html', './lesson.html', './program.html', './tour.html', './booking.html', './styles.css', './app.js', './assets/8585.png', './assets/8586.png', './assets/8618.jpg', './assets/8626.png', './assets/8662.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
